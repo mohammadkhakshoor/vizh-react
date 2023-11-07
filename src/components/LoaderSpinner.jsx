dbb2367748692f59a3147e2024f3217a76fef3ad
mohammad khakshoor <mkhbeats@gmail.com>
2023-11-07 19:11:14 +0330

    feat: make modal loader responsive

--- src/components/LoaderSpinner.jsx
+++ src/components/LoaderSpinner.jsx
@@ -3,7 +3,11 @@ import { spinner } from "../assets";
 const LoaderSpinner = () => {
   return (
     <i>
-      <img className="animate-spin" src={spinner} alt="spinner" />
+      <img
+        className="animate-spin max-sm:w-[4rem] max-sm:h-[4rem]"
+        src={spinner}
+        alt="spinner"
+      />
     </i>
   );
 };
--- src/components/SuccessMessage.jsx
+++ src/components/SuccessMessage.jsx
@@ -6,13 +6,17 @@ const SuccessMessage = ({ setIsLoading }) => {
   }
   return (
     <div
-      className="bg-white h-[70%] w-[60%] flex flex-col gap-6 items-center justify-center rounded-xl"
+      className="bg-white h-[70%] w-[60%] flex flex-col gap-6 items-center justify-center rounded-xl max-sm:w-[80%] max-sm:h-[60%]"
       onClick={handleRemoveModal}
     >
       <i>
-        <img src={successIcon} alt="success icon" />
+        <img
+          src={successIcon}
+          alt="success icon "
+          className="max-sm:w-[4rem] max-sm:h-[4rem]"
+        />
       </i>
-      <p className="text-[2.1rem] font-bold text-black">
+      <p className="text-[2.1rem] font-bold text-black max-sm:text-[1.4rem] text-center">
         Bid submitted successfully...
       </p>
     </div>
